7c9a32134f557050fa2fdbc8b8f579319587c0dd
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-22 13:44:50 -0700

    ajuste next export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,17 @@ import { ProductCard } from '@/components/product-card';
 
 export const dynamic = 'force-dynamic';
 
-async function HomePage() {
+export default async function HomePage() {
 
   const products = await getProducts();
 
   return (
-    <>
-
-      <div className='grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 mt-5'>
-        {
-          products.map((product: any) => (
-            <ProductCard product={product} key={product.id} />
-          ))
-        }
-      </div>
-    </>
+    <div className='grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 mt-5'>
+      {
+        products.map((product: any) => (
+          <ProductCard product={product} key={product.id} />
+        ))
+      }
+    </div>
   )
 }
-export default HomePage
