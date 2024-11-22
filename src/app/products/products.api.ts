c04f6193e1c13a3dccb5451e34142a6cb88e8e80
Omar Garcia <omar_yo2003@hotmail.com>
2024-11-22 15:03:47 -0700

    fix url api

--- src/app/products/products.api.ts
+++ src/app/products/products.api.ts
@@ -1,14 +1,14 @@
 export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export async function getProducts(){
-const res = await fetch(`${BACKEND_URL}/api/products`,
+const res = await fetch(`${BACKEND_URL}api/products`,
     {cache:'no-store'}
 )
     return await res.json()
 }
 
 export async function getProduct(id:string){
-    const res = await fetch(`${BACKEND_URL}/api/products/${id}`,
+    const res = await fetch(`${BACKEND_URL}api/products/${id}`,
         {cache:'no-store'}
     )
         return await res.json()
@@ -16,7 +16,7 @@ export async function getProduct(id:string){
 
 
 export async function createProduct(productData:any){
-    const res = await fetch(`${BACKEND_URL}/api/products`,{
+    const res = await fetch(`${BACKEND_URL}api/products`,{
         method: 'POST',
         headers:{
             'Content-type':'application/json'
@@ -28,15 +28,15 @@ export async function createProduct(productData:any){
 }
 
 export async function deleteProduct(id:string) {
-    const res = await fetch(`${BACKEND_URL}/api/products/${id}`,{
+    const res = await fetch(`${BACKEND_URL}api/products/${id}`,{
         method:"DELETE"
     })
     console.log(res);
 }
 
 export async function updateProduct(id:string, newProduct:any){
-    console.log(`${BACKEND_URL}/api/products/${id}`)
-    const res = await fetch(`${BACKEND_URL}/api/products/${id}`,{
+    console.log(`${BACKEND_URL}api/products/${id}`)
+    const res = await fetch(`${BACKEND_URL}api/products/${id}`,{
         method: 'PATCH',
         headers:{
             'Content-type':'application/json'
